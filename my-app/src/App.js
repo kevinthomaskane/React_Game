5451029ae4f23ce602fb9db6b7d56d57b671c957
Travis Kane <kevinthomaskane@gmail.com>
2018-04-14 19:49:43 -0700

    added methods to stateful app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,20 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/header";
 import Images from "./components/images";
+import players from "./images.json";
 
 class App extends Component {
 
   state = {
-    images: [],
+    players: [],
+    points: 0
   };
 
-  
+  shuffleArray = array => {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      this.setState({players: array}) 
+  };
+
+    handleClick = event => {
+      var choice = event.target.name;
+      var score = this.state.points;
+      for (let i = 0; i < players.length; i++){
+        if (players[i].name === choice && players[i].selected === false){
+          players[i].selected = true
+          score++
+        } else {
+          console.log("you lose")
+        }
+      }
+      this.setState({players: players, points: score})
+    };
+
+    checkWin = () => {
+      this.state.points === 12 ? console.log("you win") : console.log("you lose")
+    }
+
 
   render() {
     return (
       <div>
-        <Header />
-        <Images />
+        <Header score={this.state.points}/>
+        <Images players={this.state.players} checkWin={this.checkWin} handleClick={this.handleClick}/>
       </div>
     );
   }
